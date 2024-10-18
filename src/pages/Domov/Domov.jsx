3cfc631b8648e31dae767bdfbee0fa7b6321c166
MrKerinoo <klocokmarek@gmail.com>
2024-10-18 11:39:48 +0200

    Done homepage.

--- src/pages/Domov/Domov.jsx
+++ src/pages/Domov/Domov.jsx
@@ -11,16 +11,19 @@ import LightbulbIcon from "@mui/icons-material/Lightbulb";
 export default function Domov() {
   const services = [
     {
+      id: 1,
       heading: "Tvorba webstránok",
       text: "Tvorba internetových stránok podľa vaších predstáv alebo to môžete nechať na nás a my sa o to postaráme.",
       icon: <DevicesIcon />,
     },
     {
+      id: 2,
       heading: "Stratégia obsahu",
       text: "Naše popisy k stránkam bývajú posudzované expertmi, aby neboli zbytočne zdĺhavé a zložité, ale krátke a výstižné.",
       icon: <NewspaperIcon />,
     },
     {
+      id: 3,
       heading: "Obchodné poradenstvo",
       text: "Dokážeme Vám pomôcť s marketingom Vašej značky a následným rozvojom.",
       icon: <LightbulbIcon />,
@@ -29,53 +32,51 @@ export default function Domov() {
 
   const projects = [
     {
+      id: 1,
       name: "Pneuservis Balko",
       url: "https://www.pneuservisbalko.sk/",
       image: "/src/assets/images/pneuservisBalko.png",
     },
     {
+      id: 2,
+      name: "Pneuservis Balko",
+      url: "https://byvaj.com/",
+      image: "/src/assets/images/byvaj.png",
+    },
+    {
+      id: 3,
       name: "Zmenáreň Ružomberok",
       url: "https://www.zmenarenruzomberok.sk/",
       image: "/src/assets/images/zmenarenRuzomberok.png",
     },
-    {
-      name: "Pneuservis Balko",
-      url: "https://www.pneuservisbalko.sk/",
-      image: "/src/assets/images/pneuservisBalko.png",
-    },
   ];
 
   const servicesItems = services.map((service) => (
-    <Grid2 item xs={12} sm={4}>
+    <div key={service.id}>
       <Card heading={service.heading} text={service.text} icon={service.icon} />
-    </Grid2>
+    </div>
   ));
 
   const projectsItems = projects.map((project) => (
-    <Link to={project.url} target="_blank" rel="noopener noreferrer">
+    <Link to={project.url} key={project.id} target="_blank" rel="noopener noreferrer">
       <img src={project.image} alt={project.name} />
     </Link>
   ));
 
   return (
     <div>
-      <div className="home-container">
+      <div className="main-container">
         <h1>Moderné webové stránky</h1>
         <p>Dizajn, tvorba a údržba</p>
       </div>
 
       <div className="home-services">
-        <Grid2 container spacing="6">
-          {servicesItems}
-        </Grid2>
+        {servicesItems}
       </div>
 
       <div className="home-projects">
         <h1>Naše projekty</h1>
-
-        <div className="home-projects-images">
-          {projectsItems}
-        </div>
+        <div className="home-projects-images">{projectsItems}</div>
       </div>
     </div>
   );
--- src/pages/Kontakt/Kontakt.jsx
+++ src/pages/Kontakt/Kontakt.jsx
@@ -3,7 +3,14 @@ import React from "react";
 export default function Kontakt() {
   return (
     <div>
-      <h1>Kontakt</h1>
+      <div className="main-container">
+        <h1>Kontakt</h1>
+      </div>
+        
+      <div className="contact-form">
+        
+      </div>
     </div>
+
   );
 }
